67b6bf0e76c8fcbc1af26cbfbc65150e27991bfb
Seddek Nadhem <seddeknadhem@gmail.com>
2026-01-24 06:53:57 +0300

    add new functions to the class, add/delete expense

--- ExpenseManager.php
+++ ExpenseManager.php
@@ -11,6 +11,16 @@ class ExpenseManager {
         }
     }
 
+    public function hasExpense($id) {
+        $expenses = $this->loadExpenses();
+
+        foreach($expenses as $expense) {
+            if ($expense['id'] == $id) return true;
+        }
+
+        return false;
+    }
+
     public function loadExpenses() {
         $content = file_get_contents($this->filePath);
         $data = json_decode($content, true);
@@ -21,6 +31,57 @@ class ExpenseManager {
     public function saveExpenses(array $data) {
         $json = json_encode($data, JSON_PRETTY_PRINT);
         file_put_contents($this->filePath, $json);
+    }
+
+    public function addExpense($description, $amount) {
+        $expenses = $this->loadExpenses();
+
+        $newId = 1;
+        if (!empty($expenses)) {
+            $lastExpense = end($expenses);
+            $newId = $lastExpense['id'] + 1;
+        }
+
+        $newExpense = [
+            'id' => $newId,
+            'date' => date('Y-m-d'),
+            'description' => $description,
+            'amount' => $amount
+        ];
+
+        $expenses[] = $newExpense;
+
+        $this->saveExpenses($expenses);
+
+        return $newId;
+    }
+
+    public function deleteExpense($id) {
+        $expenses = $this->loadExpenses();
+        $newExpenses = [];
+
+        foreach($expenses as $expense) {
+            if ($expense['id'] == $id) continue;
+            else $newExpenses[] = $expense;
+        }
+
+        if (count($expenses) === count($newExpenses)) return false;
+
+        $this->saveExpenses($newExpenses);
+        
+        return true;
+    }
+
+    public function updateExpense($id, $description, $amount) {
+        $isFound = $this->hasExpense($id);
+        if (!$isFound) return "Note is Not Found!\n";
+
+        
+
+    }
+
+    public function getAllExpenses() {
+        return $this->loadExpenses();
     }
 
 }
