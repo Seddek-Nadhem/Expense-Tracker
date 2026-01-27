6e267df6e04f894b21b6d8098ec0e10c9642953b
Seddek Nadhem <seddeknadhem@gmail.com>
2026-01-27 11:20:46 +0300

    Feat: Implement summary command with strict validation and monthly filter

--- expense-tracker.php
+++ expense-tracker.php
@@ -25,7 +25,7 @@ switch ($argv[1]) {
         $expenses = $manager->loadExpenses();
         if (empty($expenses)) {
             echo "No expenses recorded yet.\n";
-            break;
+            exit(1);
         }
 
         printAllExpenses($expenses);
@@ -42,11 +42,29 @@ switch ($argv[1]) {
         else echo "Expense has not been found! Please enter the right ID\n";
         break;
 
+    case "summary":
+        if (count($argv) < 3) {
+            getSummaryOfAllMonths($manager);
+            exit(0);
+        }
+
+        validateSummaryArgs($argv);
+
+        $month = $argv[3];
+        $summary = getSummaryOfOneMonth($manager, $month);
+
+        if ($summary == 0) {
+            echo "There are no expenses for this month!\n";
+            exit(0);
+        } else {
+            echo "The summary of the month you entered is: \${$summary}\n";
+        }
+
+        break;
+
     default:
         echo "default\n";
 }
-
-
 
 function showHelp() {
     echo "Usage: expense-tracker <command> [options]\n\n";
@@ -61,6 +79,12 @@ function validateAddArgs($argv) {
     // 1. Check count
     if (count($argv) < 6) {
         echo "Error: Missing arguments.\n";
+        echo "Usage: expense-tracker add --description \"Item\" --amount 20\n";
+        exit(1);
+    }
+
+    if (count($argv) > 6) {
+        echo "You entered arguments more than needed!\n";
         echo "Usage: expense-tracker add --description \"Item\" --amount 20\n";
         exit(1);
     }
@@ -92,6 +116,12 @@ function validateDeleteArgs($argv) {
         exit(1);
     }
 
+    if (count($argv) > 4) {
+        echo "You entered arguments more than needed!\n";
+        echo "It has to be like this: expense-tracker delete --id <id>\n";
+        exit(1);
+    }
+
     if ($argv[2] !== '--id') {
         echo "Error: Expected '--id' but found '{$argv[2]}'.\n";
         exit(1);
@@ -101,6 +131,66 @@ function validateDeleteArgs($argv) {
         echo "The ID you entered is not a number. Please enter the correct ID!\n";
         exit(1);
     }
+}
+function validateSummaryArgs($argv) {
+    if (count($argv) < 4) {
+        echo "You didn't enter enough arguments!\n";
+        echo "It has to be like this: expense-tracker summary OR like this: expense-tracker summary --month 8\n";
+        exit(1);
+    }
+
+    if (count($argv) > 4) {
+        echo "You entered arguments more than needed!\n";
+        echo "It has to be like this: expense-tracker summary OR like this: expense-tracker summary --month 8\n";
+        exit(1);
+    }
+
+    if (is_numeric($argv[2]) || $argv[2] != "--month") {
+        echo "Review your input please!\n";
+        echo "It has to be like this: expense-tracker summary --month 8\n";
+        exit(1);
+    }
+
+    if (!is_numeric($argv[3])) {
+        echo "The month should be a number from 1-12!\n";
+        exit(1);
+    }
+
+    if ($argv[3] > 12 || $argv[3] < 1) {
+        echo "The month should be a number from 1-12!\n";
+        exit(1);
+    }
+}
+function getSummaryOfOneMonth($manager, $month) {
+    $expenses = $manager->loadExpenses();
+    $summary = 0;
+
+    foreach($expenses as $expense) {
+        // 1. strtotime converts "2026-01-24" into a timestamp
+        // 2. date('n') extracts the month number without leading zeros (1 to 12)
+        $expenseMonth = date('n', strtotime($expense['date']));
+
+        if ((int)$expenseMonth === (int)$month) {
+            $summary += $expense['amount'];
+        }
+    }
+
+    return $summary;
+}
+function getSummaryOfAllMonths($manager) {
+    $expenses = $manager->loadExpenses();
+
+    if (empty($expenses)) {
+        echo "There're no expenses!\n";
+        exit(1);
+    }
+
+    $summary = 0;
+    foreach($expenses as $expense) {
+        $summary += $expense['amount'];
+    }
+
+    echo "Total expenses: {$summary}\n";
 }
 function printAllExpenses($expenses) {
     // 2. Print the Header Row
